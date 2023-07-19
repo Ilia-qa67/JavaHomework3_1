bf5babae33947f14e153ac9ef3bbbd9b66c1f43c
Ilia Tsarev <bkilja@gmail.com>
2023-07-19 15:24:14 +0400

    change

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,9 @@
 public class BonusMilesService {
 
     public int calculate(int ticketPrice) {
-        int result;
-        if (ticketPrice > 1000) {
-            result = ticketPrice / 20;
-        } else {
-            result = 0;
-        }
+
+        int result = ticketPrice / 20;
+
         return result;
     }
 
